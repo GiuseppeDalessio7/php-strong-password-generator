29d5cb30a0693ad022f18974a4cd70e4cb3e1cab
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-10-12 08:28:44 +0200

    Create a form

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+/* 
+Milestone 1
+Creare un form che invii in GET la lunghezza della password.
+ Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. 
+ Scriviamo tutto (logica e layout) in un unico file index.php
+
+*/
+
+$_GET
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+</head>
+
+<body>
+
+
+    <form action="" method="get">
+
+        <input type="text" name="password" id="password">
+        <button type="submit">invia</button>
+
+    </form>
+
+</body>
+
+</html>
